d56638a9817b20e316755fa9e523d79217028ee9
Christian Elies <crelies@icloud.com>
2019-08-13 19:43:30 +0200

    Initial Commit

---
+++ Game-of-Thrones-VIPER-SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Game-of-Thrones-VIPER-SwiftUI
+//
+//  Created by Christian Elies on 13.08.19.
+//  Copyright © 2019 Christian Elies. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
